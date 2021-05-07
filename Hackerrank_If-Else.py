2acd0be79c7849c8815e14ee72108b9f9aa4b2e9
danishMario <83771485+danishMario@users.noreply.github.com>
2021-05-07 22:41:11 +0530

    Update Hackerrank_If-Else.py

--- Hackerrank_If-Else.py
+++ Hackerrank_If-Else.py
@@ -21,12 +21,10 @@ if __name__ == '__main__':
 
 if n%2==1:
     print("Weird")
+elif n%2==0 and n in range(2,6):
+    print("Not Weird")
+elif n%2==0 and n in range(6,20+1):
+    print("Weird")
 else:
-    if range(2,6):
-        print("Not Weird")
-    elif range(6, 21):
-        print("Weird")
-    else:
-        print("Not Weird")
-
+    print("Not Weird")        
         
