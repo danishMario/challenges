0b638385457817b982d776cda8f046f37b92f977
danishMario <83771485+danishMario@users.noreply.github.com>
2021-06-19 15:46:49 +0530

    Create basic data type.cpp

---
+++ basic data type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int x;
+    long z;
+    char c;
+    float floatVar;
+    double doubleVar;
+    cin >> x  >> z >> c >> floatVar >> doubleVar;
+    cout << x << endl;
+    cout << z << endl;
+    cout << c << endl;
+    cout  << fixed << floatVar <<endl;
+    cout << fixed << doubleVar<< endl; 
+    // Complete the code.
+    return 0;
+}
